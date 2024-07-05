3015a4ae1639efd11df39584f674947a13503a78
alex <alxkahovsky@gmail.com>
2024-07-05 15:41:26 +0300

    [DEVELOP] Удалил неиспользуемые файлы

--- rest_framework_cart/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter, SimpleRouter
-from .views import CartView
-
-app_name = 'rest_framework_cart'
-
-router = DefaultRouter()
-router.register(r'', CartView, basename='rest_framework_cart')
-
-urlpatterns = router.urls
